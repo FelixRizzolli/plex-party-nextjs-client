977c6f58b7470d651d079ce01c8be391610da7cb
Felix Rizzolli <felix.rizzolli@outlook.de>
2023-10-23 15:13:54 +0200

    install sass

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-mono font-black text-9xl underline">
+      <h1 className="title">
         <span className="text-primary underline">Plex </span>Party
       </h1>
     </main>
