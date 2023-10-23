8259ada14c9ed7775a9b5cc929c66d848d2b960d
Felix Rizzolli <felix.rizzolli@outlook.de>
2023-10-23 15:58:29 +0200

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="title">
-        <span className="text-primary underline">Plex </span>Party
-      </h1>
+      <div className="main-card">
+        <div>
+          <div>
+            <h2>
+              <span className="text-primary underline">Plex</span>Party
+            </h2>
+            <p>
+              The administrator can set up a server. This provides a GraphQL API, which delivers the movies and series available in the own Plex library. Thereby information from TVDB is added. In addition, the administrator receives a task if a new season or a new episode is available.
+            </p>
+            <p>
+              The Plex user can view the content of the Plex Libary and rate the quality of the content. He can also request new movies/series.
+            </p>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
