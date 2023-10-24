829744c09270dbed1d847ac029ed463a13aa700b
Felix Rizzolli <felix.rizzolli@outlook.de>
2023-10-24 16:37:26 +0200

    add profile picture

--- components/buttons.component.tsx
+++ components/buttons.component.tsx
@@ -2,6 +2,8 @@
 
 import { useSession, signIn, signOut } from 'next-auth/react';
 import styles from './buttons.component.module.scss';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export function SignInButton() {
   const { data: session, status } = useSession();
@@ -11,14 +13,25 @@ export function SignInButton() {
   }
 
   if (status === 'authenticated') {
-    return <SignOutButton />;
+    return (
+      <>
+        <Link href={'/profile'}>
+          <Image src={session.user?.image ?? '/vercel.svg'}
+            width={40} height={40} alt={session.user?.name ?? ''} className="rounded ml-2"
+          />
+        </Link>
+        <SignOutButton />
+      </>
+    );
   }
 
-  return <>
-    <button type="button" className={styles.btn} onClick={() => signIn()}>
-      Sign in
-    </button>
-  </>;
+  return (
+    <>
+      <button type="button" className={styles.btn} onClick={() => signIn()}>
+        Sign in
+      </button>
+    </>
+  );
 }
 
 export function SignOutButton() {
